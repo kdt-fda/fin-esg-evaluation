ef927588a8df6a923304bd4883c3f7a379038298
byungkyu510 <byungkyu510@gmail.com>
2026-03-18 15:29:05 +0900

    chore: 분석기 스케줄러 23시 실행

--- main_analyzer.py
+++ main_analyzer.py
@@ -74,11 +74,9 @@ def run_total_analysis_process():
         logging.critical(f"🚨 시스템 중단 발생: {str(e)}", exc_info=True)
 
 if __name__ == "__main__":
-    run_total_analysis_process()
+    schedule.every().day.at("23:00").do(run_total_analysis_process)
+    logging.info("⏰ 분석기 스케줄러: 매일 23:00에 업데이트를 시작합니다.")
 
-    #schedule.every().day.at("23:00").do(run_total_analysis_process)
-    #logging.info("⏰ 분석기 스케줄러: 매일 23:00에 업데이트를 시작합니다.")
-
-    #while True:
-    #    schedule.run_pending()
-    #    time.sleep(60)
+    while True:
+        schedule.run_pending()
+        time.sleep(60)
