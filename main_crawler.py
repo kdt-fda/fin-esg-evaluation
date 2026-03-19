526fc43893a35d071ac52f28d54ad774553baa20
byungkyu510 <byungkyu510@gmail.com>
2026-03-19 11:52:04 +0900

    chore: 스케줄러 시간 조정

--- main_crawler.py
+++ main_crawler.py
@@ -94,8 +94,8 @@ def run_total_update_process():
         logging.critical(f"🚨 시스템 중단 발생: {str(e)}", exc_info=True)
 
 if __name__ == "__main__":
-    schedule.every().day.at("22:30").do(run_total_update_process)
-    logging.info("⏰ 수집기 스케줄러: 매일 22:30에 업데이트를 시작합니다.")
+    schedule.every().day.at("22:00").do(run_total_update_process)
+    logging.info("⏰ 수집기 스케줄러: 매일 22:00에 업데이트를 시작합니다.")
 
     while True:
         schedule.run_pending()
